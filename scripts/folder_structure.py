1e49ccba11fb78e422818daabc24a272b37a0146
Sascha Häusler <saeschdivara@gmail.com>
2013-12-24 15:13:34 +0100

    Added script to get the right folder structure for the dist folder

---
+++ scripts/folder_structure.py
@@ -0,0 +1,44 @@
+import os
+
+# Public Server System
+os.mkdir("dist")
+os.mkdir("dist/Public-Server-System")
+os.mkdir("dist/Public-Server-System/debug")
+os.mkdir("dist/Public-Server-System/release")
+
+# 3rd party libraries #
+#######################
+os.mkdir("dist/3rdparty")
+
+# ArangoDB driver
+os.mkdir("dist/3rdparty/arangodb-driver")
+os.mkdir("dist/3rdparty/arangodb-driver/debug")
+os.mkdir("dist/3rdparty/arangodb-driver/release")
+
+# Tufao
+os.mkdir("dist/3rdparty/tufao")
+os.mkdir("dist/3rdparty/tufao/debug")
+os.mkdir("dist/3rdparty/tufao/release")
+
+# Grantlee
+os.mkdir("dist/3rdparty/grantlee")
+#   - defaultfilters
+os.mkdir("dist/3rdparty/grantlee/defaultfilters")
+os.mkdir("dist/3rdparty/grantlee/defaultfilters/debug")
+os.mkdir("dist/3rdparty/grantlee/defaultfilters/release")
+#   - defaulttags
+os.mkdir("dist/3rdparty/grantlee/defaulttags")
+os.mkdir("dist/3rdparty/grantlee/defaulttags/debug")
+os.mkdir("dist/3rdparty/grantlee/defaulttags/release")
+#   - i18n
+os.mkdir("dist/3rdparty/grantlee/i18n")
+os.mkdir("dist/3rdparty/grantlee/i18n/debug")
+os.mkdir("dist/3rdparty/grantlee/i18n/release")
+#   - loadertags
+os.mkdir("dist/3rdparty/grantlee/loadertags")
+os.mkdir("dist/3rdparty/grantlee/loadertags/debug")
+os.mkdir("dist/3rdparty/grantlee/loadertags/release")
+#   - scriptable-library
+os.mkdir("dist/3rdparty/grantlee/scriptable-library")
+os.mkdir("dist/3rdparty/grantlee/scriptable-library/debug")
+os.mkdir("dist/3rdparty/grantlee/scriptable-library/release")
