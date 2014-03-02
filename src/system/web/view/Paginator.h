ec0c43f3e13b757e673598f019d400c3f806fdbc
Sascha Haeusler <sascha.haeusler@netzbarkeit.ch>
2014-03-02 10:58:50 +0100

    Started paginator

--- src/system/web/view/Paginator.h
+++ src/system/web/view/Paginator.h
@@ -14,11 +14,46 @@ namespace Web
 namespace View
 {
 
-class PUBLICSERVERSYSTEMSHARED_EXPORT Paginator : public QObject
+template<typename T>
+class PUBLICSERVERSYSTEMSHARED_EXPORT Paginator
 {
-        Q_OBJECT
     public:
-        Paginator(QList<Model::AbstractModel *> & list, QObject *parent = 0);
+        inline Paginator(QList<T *> & list, int pageSize, int currentPage) :
+            m_list(list),
+            m_pageSize(pageSize),
+            m_currentPage(currentPage)
+        {}
+
+        int firstPageNumber() const {
+            return 1;
+        }
+
+        QList<int> pages() const {
+            QList<int> p;
+
+            for ( int i = 1; i < m_list.size(); i++) {
+                p.append(i);
+            }
+
+            return p;
+        }
+
+        int nextPage() {
+            if ( m_currentPage < m_list.size() ) {
+                return m_currentPage+1;
+            }
+
+            return -1;
+        }
+
+        int lastPageNumber() const {
+            return m_list.size();
+        }
+
+    protected:
+        QList<T *> m_list;
+        int m_pageSize;
+        int m_currentPage;
 };
 
 }
